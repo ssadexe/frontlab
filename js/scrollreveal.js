9d6fafed6a41da62bda5d14bbd3a486afa96a74f
ssadexe <diomin.vd13@icloud.com>
2022-02-21 00:59:31 +0300

    about page edits. Added blocks. Adaptive.

--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -3,13 +3,12 @@ ScrollReveal().reveal('.block-title', {delay: 200})
 ScrollReveal().reveal('.text', {delay: 200})
 
 //index.html
-ScrollReveal().reveal('.header', {delay: 500});
+ScrollReveal().reveal('.promo', {delay: 500});
 ScrollReveal().reveal('.statistic', {delay: 700});
 ScrollReveal().reveal('.get-started-btn', {delay: 900});
 
 //about_page.html
 ScrollReveal().reveal('#popup', {delay: 400})
-ScrollReveal().reveal('#frontenddev', {delay: 200})
-ScrollReveal().reveal('#javascriptdev', {delay: 200})
-ScrollReveal().reveal('#webdesigner', {delay: 200})
-ScrollReveal().reveal('#smm', {delay: 200})
+ScrollReveal().reveal('.member', {delay: 200})
+ScrollReveal().reveal('.image', {delay: 200})
+ScrollReveal().reveal('.soft-card', {delay: 200})
--- js/sidebar.js
+++ js/sidebar.js
@@ -3,4 +3,5 @@ let burgerBtn = document.getElementById('sidebar-btn');
 burgerBtn.onclick = function sidebar() {
     document.getElementById('sidebar').classList.toggle('sidebar-anim');
     document.getElementById('sidebar-btn').classList.toggle('sidebar-btn-anim');
+    document.querySelector('body').classList.toggle('body-fixed');
 }
