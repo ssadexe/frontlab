d241f69449f0bd7faafa2a723681442ae652b416
ssadexe <diomin.vd13@icloud.com>
2022-02-22 01:21:16 +0300

    scrollreeval fix

--- js/scroll.js
+++ js/scroll.js
@@ -3,9 +3,10 @@ ScrollReveal().reveal('.block-title', {delay: 200})
 ScrollReveal().reveal('.text', {delay: 200})
 
 //index.html
-ScrollReveal().reveal('.promo', {delay: 500});
-ScrollReveal().reveal('.statistic', {delay: 700});
-ScrollReveal().reveal('.get-started-btn', {delay: 900});
+ScrollReveal().reveal('.slogan', {delay: 500});
+ScrollReveal().reveal('.head-description', {delay: 700});
+ScrollReveal().reveal('.subslogan', {delay: 900});
+ScrollReveal().reveal('.start-btn', {delay: 1100});
 
 //about_page.html
 ScrollReveal().reveal('#popup', {delay: 400})
