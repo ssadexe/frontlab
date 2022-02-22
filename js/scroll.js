6f484b12bd29086db5a1dd9ac862cedd9884b563
ssadexe <diomin.vd13@icloud.com>
2022-02-23 02:17:12 +0300

    about page added

--- js/scroll.js
+++ js/scroll.js
@@ -2,3 +2,10 @@
 // index.html
 ScrollReveal().reveal('.main-title', { delay: 300 });
 ScrollReveal().reveal('.main-subtitle', { delay: 900 });
+ScrollReveal().reveal('.text', { delay: 200 });
+
+//about_page.html
+ScrollReveal().reveal('#popup', {delay: 400})
+ScrollReveal().reveal('.member', {delay: 200})
+ScrollReveal().reveal('.image', {delay: 200})
+ScrollReveal().reveal('.soft-card', {delay: 200})
