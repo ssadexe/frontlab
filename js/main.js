c9e457275268f254e46eae6a4c4a08b19b11d552
ssadexe <diomin.vd13@icloud.com>
2022-02-24 00:26:33 +0300

    redisign pages

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 let sidebarBtn = document.getElementById('sidebar-btn')
 let sidebar = document.getElementById('sidebar')
+let sidebarBtnIcon = document.getElementById('sidebar-btn-icon')
 
 sidebarBtn.onclick = function openSidebar() {
     sidebar.classList.toggle('sidebar-opened')
     sidebarBtn.classList.toggle('sidebar-btn-animation')
+    if (sidebar.classList.contains('sidebar-opened')) {
+        sidebarBtnIcon.classList.toggle('sidebar-icon-transition')
+        sidebarBtnIcon.src = 'img/icons/cancel.svg'
+    } else {
+        sidebarBtnIcon.src = 'img/icons/menu.svg'
+    }
 }
