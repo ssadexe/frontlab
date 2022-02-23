96807d584e64e748dc91280d377608d41248b9a7
ssadexe <diomin.vd13@icloud.com>
2022-02-24 01:47:57 +0300

    many fix and changes

--- js/scroll.js
+++ js/scroll.js
@@ -1,11 +1,12 @@
+// global
+ScrollReveal().reveal('.block');
+ScrollReveal().reveal('.block-title', { delay: 200 });
+ScrollReveal().reveal('.text', { delay: 200 });
 
 // index.html
 ScrollReveal().reveal('.main-title', { delay: 300 });
 ScrollReveal().reveal('.main-subtitle', { delay: 900 });
-ScrollReveal().reveal('.text', { delay: 200 });
 
 //about_page.html
-ScrollReveal().reveal('#popup', {delay: 400})
-ScrollReveal().reveal('.member', {delay: 200})
-ScrollReveal().reveal('.image', {delay: 200})
-ScrollReveal().reveal('.soft-card', {delay: 200})
+ScrollReveal().reveal('.team__developer', {delay: 200})
+ScrollReveal().reveal('.instruments__card', {delay: 200})
