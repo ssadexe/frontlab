411fa459534e81cc74e5e83cb630db47dfff5d06
ssadexe <diomin.vd13@icloud.com>
2022-02-24 23:13:43 +0300

    changed index.css

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let sidebarBtn = document.getElementById('sidebar-btn')
+let sidebar = document.getElementById('sidebar')
+let sidebarBtnIcon = document.getElementById('sidebar-btn-icon')
+
+sidebarBtn.onclick = function openSidebar() {
+    sidebar.classList.toggle('sidebar-opened')
+    sidebarBtn.classList.toggle('sidebar-btn-animation')
+    if (sidebar.classList.contains('sidebar-opened')) {
+        sidebarBtnIcon.src = 'img/icons/cancel.svg'
+    } else {
+        sidebarBtnIcon.src = 'img/icons/menu.svg'
+    }
+}
