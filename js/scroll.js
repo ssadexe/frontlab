500553fecfc8510a862b905c61ffb53958249b72
ssadexe <diomin.vd13@icloud.com>
2022-02-24 23:08:42 +0300

    added services page

--- js/scroll.js
+++ js/scroll.js
@@ -1,7 +1,5 @@
 // global
 ScrollReveal().reveal('.block');
-ScrollReveal().reveal('.block-title', { delay: 200 });
-ScrollReveal().reveal('.text', { delay: 200 });
 
 // index.html
 ScrollReveal().reveal('.main-title', { delay: 300 });
