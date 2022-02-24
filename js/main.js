5fe5e2e9dd7d6acfb42f39510ac800ae3563b133
ssadexe <diomin.vd13@icloud.com>
2022-02-25 01:25:39 +0300

    changed colors and add back button in navigation bar

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 let sidebarBtn = document.getElementById('sidebar-btn')
 let sidebar = document.getElementById('sidebar')
 let sidebarBtnIcon = document.getElementById('sidebar-btn-icon')
+let backBtn = document.getElementById('back-btn')
 
 sidebarBtn.onclick = function openSidebar() {
     sidebar.classList.toggle('sidebar-opened')
     sidebarBtn.classList.toggle('sidebar-btn-animation')
     if (sidebar.classList.contains('sidebar-opened')) {
         sidebarBtnIcon.src = 'img/icons/cancel.svg'
+        document.body.style.position = 'fixed'
     } else {
         sidebarBtnIcon.src = 'img/icons/menu.svg'
+        document.body.style.position = 'relative'
     }
+}
+
+backBtn.onclick = function backPage() {
+    history.back()
 }
