702455399653bb5049e07ec3f433dc637745f651
ssadexe <diomin.vd13@icloud.com>
2022-02-26 00:37:35 +0300

    new color pallete and UI elements

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
-let sidebarBtn = document.getElementById('sidebar-btn')
-let sidebar = document.getElementById('sidebar')
-let sidebarBtnIcon = document.getElementById('sidebar-btn-icon')
-let backBtn = document.getElementById('back-btn')
+let sidebarBtn = document.getElementById('sidebar-btn');
+let sidebar = document.getElementById('sidebar');
+let sidebarBtnIcon = document.getElementById('sidebar-btn-icon');
+let backBtn = document.getElementById('back-btn');
 
 sidebarBtn.onclick = function openSidebar() {
-    sidebar.classList.toggle('sidebar-opened')
-    sidebarBtn.classList.toggle('sidebar-btn-animation')
+    sidebar.classList.toggle('sidebar-opened');
+    sidebarBtn.classList.toggle('sidebar-btn-animation');
+
     if (sidebar.classList.contains('sidebar-opened')) {
-        sidebarBtnIcon.src = 'img/icons/cancel.svg'
-        document.body.style.position = 'fixed'
+        sidebarBtnIcon.src = 'img/icons/UI/cancel.svg';
+        document.body.style.position = 'fixed';
     } else {
-        sidebarBtnIcon.src = 'img/icons/menu.svg'
-        document.body.style.position = 'relative'
+        sidebarBtnIcon.src = 'img/icons/UI/menu.svg';
+        document.body.style.position = 'relative';
     }
 }
 
 backBtn.onclick = function backPage() {
-    history.back()
+    sidebar.classList.toggle('sidebar-opened')
+    history.back();
 }
